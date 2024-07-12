59e6d73c3beb104685596e955bb7f6fa638fe55c
NAUT5 <Mohamedtlili1995@hotmail.com>
2024-07-12 13:45:56 +0100

    first commit

---
+++ routes/user/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const route = express.Router();
+const verifiedToken = require("../../middlewares/verifyToken");
+const upload = require("../../middlewares/multer");
+
+// register
+route.post("/register", require("./register"));
+// login
+route.post("/login", require("./login"));
+
+// update user photo
+route.put(
+  "/updatePhoto",
+  verifiedToken,
+  upload.single("photo"),
+  require("./updatePhoto")
+);
+
+// get information
+route.get("/getInformation", verifiedToken, require("./GetInformation"));
+
+// update information
+route.put("/updateInformation", verifiedToken, require("./updateInformation"));
+
+// get products
+route.get("/getProducts", require("./getProducts"));
+
+// get single product
+route.get("/getSingleProduct/:id", require("./getSingleProduct"));
+
+
+
+
+
+
+
+
+
+
+
+module.exports = route;
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const cors = require("cors");
+const path = require("path");
+
+require("dotenv").config();
+
+// environmentals varibales
+const DBURI = process.env.DBURI;
+const PORT = process.env.PORT || 5000;
+
+// function
+mongoose
+  .connect(DBURI)
+  .then(() => {
+    console.log("database connected ✅");
+  })
+  .catch((err) => {
+    console.log(err);
+    console.log("can't coonect to database ❌");
+  });
+// middlewares
+//ihez data mel clinet ya3mlelha pass o isobha fel body
+app.use(express.json());
+app.use(cors());
+
+// // api routes
+// app.use("/api/user", require("./routes/user"));
+// // user routes
+// app.use("/api/admin", require("./routes/admin"));
+// // admin routes
+// app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+// // path fille uplod bech isob fi
+
+// listen to requests
+app.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`server is up and runing on port :${PORT}`);
+});
