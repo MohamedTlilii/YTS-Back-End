76de7dbeebc200f6bf68ef131b3665db83f6071a
NAUT5 <Mohamedtlili1995@hotmail.com>
2024-08-02 11:39:39 +0100

    server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 require("dotenv").config();
-
+// 
 
 const cors = require("cors");
 const path = require("path");
